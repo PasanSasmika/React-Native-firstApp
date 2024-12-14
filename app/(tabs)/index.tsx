d52e3be62de8291eff30f7966f4c92707197e643
PasanSasmika <pasansasmika198@gmail.com>
2024-12-14 07:49:15 +0530

    Add Background image and welcome text 🙆

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View, Image } from "react-native";
 
  function HomeScreen() {
 
   return (
    
-  <View style={{ backgroundColor: 'yellow', flex:1}}> 
+  <View style={{ backgroundColor: 'white', flex:1}}> 
+  <Image style={{width:'100%', height:'100%',position:'absolute'}} source={require('../../assets/images/bg.jpg')}/>
+
+  <Text style={{fontSize:45, color:'white', fontWeight:'600', marginTop:100 , marginLeft:20}}>{'Welcome\nBack'}</Text>
     
   </View>
   );  
