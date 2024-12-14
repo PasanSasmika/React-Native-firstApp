c05b35095adb0fb300431d2a5a47f00777cf75b5
PasanSasmika <pasansasmika198@gmail.com>
2024-12-14 08:06:40 +0530

    Create login and signup field 👍✨

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,20 @@
 import { Button, Text, TextInput, View, Image } from "react-native";
 
+function LoginField(){
+  return(
+    <View style={{marginTop:120}} >
+      <View style={{backgroundColor: 'white', borderRadius:20, height:65, marginHorizontal:30, justifyContent:'center',paddingLeft:20}}>
+        <TextInput placeholder="Your Email" placeholderTextColor={'#000'} style={{fontSize:18}}/>
+      </View>
+      <View style={{backgroundColor: 'white', borderRadius:20, height:65,  marginTop:16,marginHorizontal:30, justifyContent:'center',paddingLeft:20}}>
+        <TextInput placeholder="Your Password" placeholderTextColor={'#000'} style={{fontSize:18}}/>
+      </View>
+    </View>
+  );
+}
+
+
+
  function HomeScreen() {
 
   return (
@@ -7,7 +22,11 @@ import { Button, Text, TextInput, View, Image } from "react-native";
   <View style={{ backgroundColor: 'white', flex:1}}> 
   <Image style={{width:'100%', height:'100%',position:'absolute'}} source={require('../../assets/images/bg.jpg')}/>
 
-  <Text style={{fontSize:45, color:'white', fontWeight:'600', marginTop:100 , marginLeft:20}}>{'Welcome\nBack'}</Text>
+  <Text style={{fontSize:45, color:'white', fontWeight:'600', marginTop:100 , marginLeft:20}}>{'Welcome\n Back'}</Text>
+
+  <LoginField/>
+
+   
     
   </View>
   );  
